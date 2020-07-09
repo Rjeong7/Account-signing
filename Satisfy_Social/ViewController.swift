02246f5972ce51c0fbc19c6220f6e5872d547590
Richard Jeong <richard.jeong7@gmail.com>
2020-07-09 16:20:47 -0700

    Initial Commit

---
+++ Satisfy_Social/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Satisfy_Social
+//
+//  Created by Richard Jeong on 7/9/20.
+//  Copyright © 2020 Richard Jeong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
